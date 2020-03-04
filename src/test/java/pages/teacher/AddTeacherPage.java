654e83ee96c85f81c4af0a4e8ee028a73885818b
ulanovka27 <56323090+ulanovka27@users.noreply.github.com>
2020-03-04 16:49:14 -0600

    committing elements

--- src/test/java/pages/teacher/AddTeacherPage.java
+++ src/test/java/pages/teacher/AddTeacherPage.java
@@ -15,28 +15,63 @@ public class AddTeacherPage {
     public WebElement schoolText;
 
     @FindBy (xpath = "//a[@class='subdrop']")
-    public WebElement teachersButton;
+    public WebElement teachersButtonDropdown;
 
     @FindBy (linkText = "Add Teacher")
     public WebElement addTeacherButton;
 
     @FindBy (xpath = "(//input[@type='text'])[1]")
-    public WebElement firstNameButton;
+    public WebElement firstNameInput;
 
     @FindBy (xpath = "(//input[@type='text'])[2]")
-    public WebElement emailButton;
+    public WebElement emailInput;
 
     @FindBy (xpath = "(//input[@type='password'])[1]")
-    public WebElement passwordButton;
+    public WebElement passwordInput;
 
     @FindBy (xpath = "(//input[@type='text'])[3]")
-    public WebElement subjectButton;
+    public WebElement subjectInput;
 
     @FindBy (xpath = "(//div[@class='form-group custom-mt-form-group'])[5]//select")
     public WebElement genderDropdown;
 
     @FindBy (xpath = "(//input[@type='text'])[4]")
-    public WebElement birthDate;
+    public WebElement birthDateInput;
+
+    @FindBy (xpath = "(//div[@class='form-group custom-mt-form-group'])[7]//select")
+    public WebElement batchDropdown;
+
+    @FindBy (xpath = "(//input[@type='text'])[5]")
+    public WebElement lastNameInput;
+
+    @FindBy (xpath = "(//input[@type='text'])[6]")
+    public WebElement joiningDateInput;
+
+    @FindBy (xpath = "(//input[@type='password'])[2]")
+    public WebElement confirmPasswordInput;
+
+    @FindBy (xpath = "//input[@type='number']")
+    public WebElement mobileNumberInput;
+
+    @FindBy (xpath = "(//div[@class='form-group custom-mt-form-group'])[12]/select")
+    public WebElement departmentDropdown;
+
+    @FindBy (xpath = "(//input[@type='text'])[7]")
+    public WebElement salaryInput;
+
+    @FindBy (xpath = "(//input[@type='text'])[8]")
+    public WebElement sectionInput;
+
+    @FindBy (css = "textarea#message")
+    public WebElement permanentAddressInput;
+
+    @FindBy (xpath = "//button[@type='submit']")
+    public WebElement submitButton;
+
+
+
+
+
 
 
 
